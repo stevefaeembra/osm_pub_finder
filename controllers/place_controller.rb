038bf5fbf7cc90bcfe7b72f33063578fcd56cb5c
Steven Kay <stevendkay@gmail.com>
2018-11-15 21:14:40 +0000

    allow case insensitive searches

--- controllers/place_controller.rb
+++ controllers/place_controller.rb
@@ -4,6 +4,8 @@ require_relative('../models/place')
 also_reload('../models/*')
 
 get '/find' do
-  @places = Place.search(params[:pattern])
+  @places = Place.search(params[:pattern].downcase)
+  @pattern = params[:pattern]
+  @count = @places.length
   erb(:results)
 end
