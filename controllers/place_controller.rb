f1f5709342787d5d30076e2d3b960ff7cd6703d4
Steven Kay <stevendkay@gmail.com>
2018-11-17 19:23:09 +0000

    added google-style query timings

--- controllers/place_controller.rb
+++ controllers/place_controller.rb
@@ -5,7 +5,9 @@ also_reload('../models/*')
 
 get '/find' do
   # find places matching name pattern
+  tick = Time.now
   @places = Place.search(params[:pattern].downcase)
+  @took = Time.now - tick
   @pattern = params[:pattern]
   @count = @places.length
   erb(:results)
@@ -13,7 +15,9 @@ end
 
 get '/find_pub' do
   # find pub matching name pattern
+  tick = Time.now
   @places = Place.search_pub(params[:pattern].downcase)
+  @took = Time.now - tick
   @pattern = params[:pattern]
   @count = @places.length
   erb(:pubs_by_name)
@@ -21,18 +25,22 @@ end
 
 get '/neighbours/:id' do
   # find places within a certain distance
+  tick = Time.now
   @place = Place.find(params[:id])
   @placename = @place.name
   @places = @place.find_nearby_places()
+  @took = Time.now - tick
   @count = @places.length
   erb(:neighbours)
 end
 
 get '/pubs/:id' do
   # find pubs within a certain distance of a location
+  tick = Time.now
   @place = Place.find(params[:id])
   @placename = @place.name
   @places = @place.find_pubs()
+  @took = Time.now - tick
   @count = @places.length
   erb(:pubs)
 end
